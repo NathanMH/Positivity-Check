f9e21f4b448f2d4bae5acffbf3107d4846d8b040
NathanMH <nathan@musicnate.ca>
2015-02-11 20:26:56 -0500

    Implement negative word count
    Implement user file to array of words conversion
    Negative word count works

--- PositivityCheck.py
+++ PositivityCheck.py
@@ -3,12 +3,32 @@ Author: Nathan Mador-House
 Title: PositivityCheck
 ####################"""
 
-doc = open("wordList.txt", "r")
-words = []
-instances = []
+negativeWordsDoc = open("wordList.txt", "r")
+negativeWordsArray = []
+userWordsArray = []
+negativeWordCount = 0
 
-for line in doc:
-    words.append(line)
+# User file actually from the user... Commented for testing
+# userFile = open(input("Which file would you like to analyze?: "))
 
-userFile = open(input("Which file would you like to analyze?: "))
+# Make the array of negative words
+for word in negativeWordsDoc:
+    negativeWordsArray.append(word.rstrip('\n'))
 
+
+# Make an array of words out of the user provided file
+userFile = open("testFile.txt")
+for lines in userFile:
+    for word in lines.split(" "):
+        userWordsArray.append(word.lower())
+
+
+# Go through each negative word and check if it matches any in the user file word list
+for negativeWord in negativeWordsArray:
+    for userWord in userWordsArray:
+        if negativeWord == userWord:
+            negativeWordCount += 1
+            print(negativeWord)
+
+
+print(negativeWordCount)
